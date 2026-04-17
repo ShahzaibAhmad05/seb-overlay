47bf26888818b837773c46e955c866ca92e40a8d
ShahzaibAhmad05 <shahzaibahmad6789@gmail.com>
2026-04-17 16:52:24 +0500

    add shared module accessible to all comps

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+Config object used to store and access the configuration of this project.
+"""
+
+class Config:
+    def __init__(self):
+        pass
+    
---
+++ shared/exceptions.py
@@ -0,0 +1,10 @@
+"""
+Custom exceptions to be used by all modules.
+"""
+
+
+class EnvNotConfiguredException(Exception):
+    """
+    Raise when the environment variables are not configured.
+    """
+    
